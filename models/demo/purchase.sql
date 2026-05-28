a7cb3df640dab8d77fd7f8e638cd028f8d4fc432
Joshua Silverstein <joshuasilverstein694@gmail.com>
2026-05-28 18:59:14 +0000

    dbt project setup for aws glue

--- models/demo/purchase.sql
+++ models/demo/purchase.sql
@@ -5,21 +5,22 @@
         incremental_strategy = 'merge',
         unique_key = 'PURCHASE_ID',
         merge_exclude_columns = ['INSERT_DTS']
-
     )
 }}
-
 with purchase_src as
 (
-    select 
-    PURCHASE_ID, 
-    PURCHASE_DATE, 
-    PURCHASE_STATUS, 
+    select
+    PURCHASE_ID,
+    PURCHASE_DATE,
+    PURCHASE_STATUS,
     CREATED_AT,
-    CURRENT_TIMESTAMP AS INSERT_DTS,
-    CURRENT_TIMESTAMP AS UPDATE_DTS
+    CURRENT_TIMESTAMP as INSERT_DTS,
+    CURRENT_TIMESTAMP as UPDATE_DTS
+    from {{source('purchase','PURCHASE_SRC')}}
 
-    from {{source,('purchase', 'PURCHASE_SRC')}}
+
+    {% if is_incremental() %}
+    where CREATED_AT > (select max(UPDATE_DTS) from {{this}})
+    {% endif %}
 )
-
-select * from purchase src
+select * from purchase_src
